91fd6b4e5b7bd76b32661dc088fb46400bd72b2e
Hudson Reynolds <73318449+HudsonReynolds@users.noreply.github.com>
2026-08-27 11:06:52 -0400

    update with hatched lines in constraint plots

--- InitialVehicleSizingConstraint.m
+++ InitialVehicleSizingConstraint.m
@@ -6,16 +6,19 @@
 % these will need to be justified in our document and edited based off
 % those decisions:
 
-CL_max = 1.2; % [-]
-L_D = 12;     % [-]
-V_s = 8;      % [m⋅s⁻¹]
-AR = 6;       % [-]
-e = 0.7;      % [-]
-eta_p = 0.6;  % [-]
-Cd0 = 0.05;   % [-]
+% stall speed less than 11 meters per second
+
+CL_max = 1.2; % [-], max lift coefficient
+L_D = 7.332;  % [-], lift to drag ratio
+V_s = 10;     % [m⋅s⁻¹], stall speed
+AR = 6;       % [-], aspect ratio
+e = 0.7;      % [-], spanwise efficiency
+eta_p = 0.6;  % [-], propeller efficiency
+Cd0 = 0.04;   % [-], whole aircraft
 
 % known quantities:
-rho = 1.225;  % [kg⋅m⁻³]
+rho = 1.225;  % [kg⋅m⁻³], air density
+gamma = 5;    % [deg], flight path angle in climb
 
 %% First Constraint: Stall Speed
 % this determines the range of values to consider for W/S:
@@ -28,18 +31,22 @@ Phi = 0.75; % throttle setting
 
 V_c = 1.7*V_s;
 
-W_P = (eta_p*Phi / (0.5*1.1*Cd0*rho*V_c^3)) * W_S;
+W_P_cruise = (eta_p*Phi / (0.5*1.1*Cd0*rho*V_c^3)) * W_S;
 
 % plot the result:
 figure();
-plot(W_S,W_P)
+boundaryline(W_S,W_P_cruise, 'r', 'LineWidth', 1.5,'HatchAngle',80, 'HatchLength',0.02);
 xlabel("$\frac{W}{S} \left[\frac{N}{m^2}\right]$")
 ylabel("$\frac{W}{P} \left[\frac{N}{W}\right]$", 'Rotation', 0)
 title('Sizing for Cruise Speed')
 
 
-
 %% Third Constraint: Climb Requirement
+
+% climb at 0.866 L/D max:
+L_D_climb = 0.866*L_D;
+
+%W_P_climb = eta_p / 
 
 %% Fourth Constraint: Maneuver Requirement
 
