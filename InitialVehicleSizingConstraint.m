3aa076e6be8b692a5ec03b022b96365334c5670e
Hudson Reynolds <73318449+HudsonReynolds@users.noreply.github.com>
2026-08-27 11:21:36 -0400

    update bullshit

--- InitialVehicleSizingConstraint.m
+++ InitialVehicleSizingConstraint.m
@@ -6,18 +6,19 @@
 % these will need to be justified in our document and edited based off
 % those decisions:
 
-% stall speed less than 11 meters per second
+% aircraft aerodynamic quantities
 
 CL_max = 1.2; % [-], max lift coefficient
-L_D = 7.332;  % [-], lift to drag ratio
+L_D_max = 7;  % [-], lift to drag ratio
 V_s = 10;     % [m⋅s⁻¹], stall speed
 AR = 6;       % [-], aspect ratio
 e = 0.7;      % [-], spanwise efficiency
 eta_p = 0.6;  % [-], propeller efficiency
 Cd0 = 0.04;   % [-], whole aircraft
 
-% known quantities:
+% environment quantities:
 rho = 1.225;  % [kg⋅m⁻³], air density
+mu = 0.4;     % [-] friction coeff of runway
 gamma = 5;    % [deg], flight path angle in climb
 
 %% First Constraint: Stall Speed
@@ -33,24 +34,35 @@ V_c = 1.7*V_s;
 
 W_P_cruise = (eta_p*Phi / (0.5*1.1*Cd0*rho*V_c^3)) * W_S;
 
-% plot the result:
-figure();
-boundaryline(W_S,W_P_cruise, 'r', 'LineWidth', 1.5,'HatchAngle',80, 'HatchLength',0.02);
-xlabel("$\frac{W}{S} \left[\frac{N}{m^2}\right]$")
-ylabel("$\frac{W}{P} \left[\frac{N}{W}\right]$", 'Rotation', 0)
-title('Sizing for Cruise Speed')
-
 
 %% Third Constraint: Climb Requirement
 
 % climb at 0.866 L/D max:
-L_D_climb = 0.866*L_D;
+L_D_climb = 0.866*L_D_max;
 
 %W_P_climb = eta_p / 
 
 %% Fourth Constraint: Maneuver Requirement
 
 %% Fifth Constaint: Takeoff Requirement
+
+V_to = 1.2 * V_s;
+
+numer = 1 - exp(0.6*rho);
+
+
+% Final constraint plot:
+figure();
+boundaryline(W_S,W_P_cruise, 'r', 'LineWidth', 1.5,'FlipBoundary','on', 'DisplayName', 'Cruise Constraint');
+hold on;
+boundaryline(W_S_max*ones(2,1),[0;0.25], 'b', 'LineWidth', 1.5, 'DisplayName', 'Stall Speed')
+
+xlabel("$\frac{W}{S} \left[\frac{N}{m^2}\right]$")
+ylabel("$\frac{W}{P} \left[\frac{N}{W}\right]$", 'Rotation', 0)
+title('Sizing for Cruise Speed')
+legend('Location','northwest')
+
+
 
 
 
