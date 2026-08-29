483021d4061a884e9098868ddf4e56b50e7d3bcd
atnery <andrewtnery@gmail.com>
2026-08-28 22:07:03 -0400

    'penis'

--- InitialVehicleSizingConstraint.m
+++ InitialVehicleSizingConstraint.m
@@ -7,14 +7,16 @@
 % those decisions:
 
 % aircraft aerodynamic quantities
-
 CL_max = 1.2; % [-], max lift coefficient
 L_D_max = 7;  % [-], lift to drag ratio
 V_s = 10;     % [m⋅s⁻¹], stall speed
 AR = 6;       % [-], aspect ratio
 e = 0.7;      % [-], spanwise efficiency
 eta_p = 0.6;  % [-], propeller efficiency
-CD_0 = 0.04;   % [-], whole aircraft
+CD_0 = 0.04;  % [-], whole aircraft drag coefficient
+n = 20;       % [-], load factor
+V_s_m = V_s * sqrt(n);
+CL_C = L_D_max * CD_0;
 
 % environment quantities:
 rho = 1.225;  % [kg⋅m⁻³], air density
@@ -35,21 +37,22 @@ V_c = 1.7*V_s;
 
 W_P_cruise = (eta_p*Phi / (0.5*1.1*CD_0*rho*V_c^3)) * W_S;
 
-
 %% Third Constraint: Climb Requirement
 
 % climb at 0.866 L/D max:
 L_D_climb = 0.866*L_D_max;
-
-%W_P_climb = eta_p / 
+W_P_climb = eta_p / (V_s * (1 / L_D_climb + sin((pi/180) * gamma)));
 
 %% Fourth Constraint: Maneuver Requirement
+q_m = 0.5 * rho * V_s_m^2;
+W_P_m = eta_p / (q_m*V_m*((CD_0/W_S) + (1/(pi*AR*e))*(n/q_m)^2*W_S));
 
 %% Fifth Constaint: Takeoff Requirement
-
-V_to = 1.2 * V_s;
+V_to = 1.1 * V_s;
 S_to = 25; % [m], takeoff distance
 
+CL_TO = CL_C;
+CD_TO = CD_0 + K * CL_TO;
 %CD_G = (CD_TO - mu*CL_TO);
 
 %numer = 1 - exp(0.6*rho*g*CD_G*S_TO*1./(W_S));
