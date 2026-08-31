f49e66d336e5c5adebfa55d46d5f731bb0775339
atnery <andrewtnery@gmail.com>
2026-08-30 21:44:34 -0400

    updated diagram

--- InitialVehicleSizingConstraint.m
+++ InitialVehicleSizingConstraint.m
@@ -38,7 +38,6 @@ W_P_TO = (numer ./ denom) * (data.eta_p_TO / data.V_TO);
 
 
 %% Final constraint plot:
-
 % create a filter to show the feasible region
 W_P_climb_vec = W_P_climb * ones(size(W_S));
 lower_envelope = min([W_P_climb_vec(:), W_P_cruise(:), W_P_m(:), W_P_TO(:)], [], 2);
@@ -57,23 +56,27 @@ fill([x_fill; flipud(x_fill)], [y_fill; zeros(size(y_fill))], [0.8 0.6 0.7], ...
 hold on;
 
 % Plot constraint lines
-boundaryline(W_S_max*ones(2,1),[0;0.25], 'r', 'LineWidth', 1.5, 'DisplayName', 'Stall Speed');
-boundaryline([0;W_S_max],W_P_climb*ones(2,1), 'g', 'LineWidth', 1.5,'FlipBoundary','on', 'DisplayName', 'Climb Constraint');
-boundaryline(W_S,W_P_cruise, 'b', 'LineWidth', 1.5,'FlipBoundary','on', 'DisplayName', 'Cruise Constraint');
-boundaryline(W_S,W_P_m, 'c', 'LineWidth', 1.5,'FlipBoundary','on', 'DisplayName', 'Manuever Constraint');
-boundaryline(W_S,W_P_TO, 'k', 'LineWidth', 1.5,'FlipBoundary','on', 'DisplayName', 'Takeoff Constraint');
+plot(W_S_max*ones(2,1),[0;0.25], 'r-',  'LineWidth', 1.5, 'DisplayName', 'Stall Speed');
+plot([0;W_S_max],W_P_climb*ones(2,1), 'g--', 'LineWidth', 1.5, 'DisplayName', 'Climb Constraint');
+plot(W_S,W_P_cruise, 'b:',  'LineWidth', 1.5, 'DisplayName', 'Cruise Constraint');
+plot(W_S,W_P_m, 'c-.', 'LineWidth', 1.5, 'DisplayName', 'Manuever Constraint');
+plot(W_S,W_P_TO, 'k-',  'LineWidth', 2.5, 'DisplayName', 'Takeoff Constraint');
 plot(data.W_S_design,data.W_P_design, 'r*', 'MarkerSize', 12, 'DisplayName', 'Design Point')
+text(data.W_S_design-1, data.W_P_design-.001, ...
+    sprintf('(%.1f, %.3f)', data.W_S_design, data.W_P_design), ...
+    'VerticalAlignment', 'top', 'HorizontalAlignment', 'right', ...
+    'FontSize', 12, 'Color', 'r')
 
 ylim([0, 0.50]);
 xlim([0, W_S_max*1.05]);
+set(gca, 'FontSize', 14);
+xlabel("$\frac{W}{S} \left[\frac{N}{m^2}\right]$", 'FontSize', 18)
+ylabel("$\frac{W}{P} \left[\frac{N}{W}\right]$", 'Rotation', 0, 'FontSize', 18)
+title('Aircraft Constraint Diagram', 'FontSize', 18)
+legend('Location','northeast', 'FontSize', 10)
 
-
-xlabel("$\frac{W}{S} \left[\frac{N}{m^2}\right]$")
-ylabel("$\frac{W}{P} \left[\frac{N}{W}\right]$", 'Rotation', 0)
-title('Sizing for Cruise Speed')
-legend('Location','northeast')
-
-
+set(gcf, 'Units', 'inches', 'Position', [1 1 11 8]);
+exportgraphics(gcf, 'ConstraintDiagram.png', 'Resolution', 300);
 
 
 
