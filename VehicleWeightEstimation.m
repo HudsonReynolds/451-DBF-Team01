200cf5db5a340989ccf4ddf2b8cf4eb05c92ecb9
Hudson Reynolds <73318449+HudsonReynolds@users.noreply.github.com>
2026-09-01 23:30:10 -0400

    bullshit

--- VehicleWeightEstimation.m
+++ VehicleWeightEstimation.m
@@ -1,5 +1,6 @@
 % This script does initial sizing of the vehicle based on constraint
 % analysis for the system.
+close all;clear;clc
 
 % system parameters are all pulled from an Excel spreadsheet. See
 % 'SizingParams.xlsx' for more details:
@@ -23,7 +24,7 @@ rho_battery = 5.27E5; %Joule / kg
 eta_m = 0.7; % revisit motor efficiency
 P_m = eta_m * 1000; % revisit, might determined based off W/P; motor efficiency times 1kW battery
 
-T_CL = sqrt(100^2+30^2) / (data.V_C); 
+T_CL = sqrt(100^2+30^2) / (data.V_TO); 
 T_TO = 25 / data.V_TO * 2; % multiple by 2 for average speed during accelerating takeoff.
 warmupN = 10;
 
@@ -42,22 +43,72 @@ ClimbBatteryWeightFraction = data.V_TO * T_CL * data.g * (cos(deg2rad(data.gamma
 TakeoffEnergyRequired = P_m / eta_m * T_TO;
 
 %% Battery Weight Fraction for Takeoff
-TakeOffBatteryWeightFraction = T_TO * data.g / (eta_m * (data.W_P_design) * rho_battery); %gravity??
+TakeOffBatteryWeightFraction = T_TO * data.g / (eta_m * data.eta_p_TO * (data.W_P_design) * rho_battery); %gravity??
 
 %% Battery Weight Fraction for Warmup
-WarmUpBatteryWeightFraction = warmupN * TakeOffBatteryWeightFraction; %clarify /W or not
-
-WeightFraction = .65; % Guess based on Chart in A3 assignment
+WarmUpBatteryWeightFraction = warmupN * TakeOffBatteryWeightFraction; %clarify W or not
 
 
-BatteryWeightFraction = WarmUpBatteryWeightFraction + ...
+
+BatteryWeightFractionPlane = WarmUpBatteryWeightFraction + ...
     TakeOffBatteryWeightFraction + ClimbBatteryWeightFraction + ...
-    TurningBatteryWeightFraction + LevelFlightBatteryWeightFraction
+    TurningBatteryWeightFraction + LevelFlightBatteryWeightFraction;
 
-%WeightOfBattery = BatteryWeightFraction * VehicleWeight
+% battery 20% margin:
+useableCapacity = 0.8;
+
+BatteryWeightFraction = 1/useableCapacity * BatteryWeightFractionPlane;
+
+% temperature derate (table based on temperature, have operating temp as an
+% input?)
+
+temp_derate = 0.7; % find a manual for the selected battery
+BatteryWeightFraction = 1/temp_derate * BatteryWeightFraction;
 
 
+% plot over a range of weight values to find the solution:
+W_pay = 0.52; % [kg]
 
+W = linspace(1,7,100); % range of values [kg]
+
+W_batt_payload = (BatteryWeightFraction)*W + W_pay;
+
+
+W_We = W - data.W_e_frac*W;
+
+% find the intersection of these lines for the empty weight:
+[~,idx] = min(abs(W_batt_payload-W_We));
+Weight = W(idx);
+Weight_y = W_We(idx);
+
+figure();
+plot(W,W_batt_payload,'g', 'DisplayName', '$W_B + W_P$')
+hold on
+plot(W,W_We,'b', 'DisplayName', '$W - W_e$')
+plot(Weight,Weight_y,'ro','DisplayName',string(Weight))
+xlabel('Total Weight [kg]')
+ylabel('Fractional Weights [kg]')
+title('Weight Estimate')
+legend('Location','Best');
+
+% use total weight for energies
+totBatteryWeight = BatteryWeightFraction * Weight
+totBatteryEnergy = totBatteryWeight * rho_battery
+
+totEnergyRequiredByPlane = BatteryWeightFractionPlane * Weight * rho_battery
+
+totEnergyRequiredByBatt = totEnergyRequiredByPlane / (data.eta_p*eta_m)
+
+energyLost = (1-useableCapacity*temp_derate)*totEnergyRequiredByBatt
+
+energyLossPercentage = energyLost/totEnergyRequiredByBatt * 100
+
+% Delivarable 5 shit:
+
+pieChart_vals = [W_pay,totBatteryWeight,Weight - W_pay - totBatteryWeight];
+
+figure();
+piechart(pieChart_vals,["Payload Weight","Battery Weight", "Empty Weight"])
 
 
 
