be8d09f4dbbbded5e7dcee66245cc710b1e99871
atnery <andrewtnery@gmail.com>
2026-09-12 16:35:49 -0400

    updated drag polar plots

--- DragBuildUp.m
+++ DragBuildUp.m
@@ -64,5 +64,11 @@ CD_0_comp = CD_0_fuselage + CD_0_wing + CD_0_hstab + CD_0_vstab + CD_0_gear;
 CD_0_misc = data.CD_0_misc_factor * CD_0_comp
 CD_0 = CD_0_comp + CD_0_misc
 
+%% L/D
+
+L_D_cruise = data.CL_C / (CD_0 + data.K_wing*data.CL_C^2)
+L_D = 
+L_D_max = 1 / (2*sqrt(data.K_wing * CD_0))
+
 end
 
