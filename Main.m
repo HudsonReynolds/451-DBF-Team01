36fec628aaf9e2d21fe4300026152dbd9a344b6c
atnery <andrewtnery@gmail.com>
2026-09-18 18:07:10 -0400

    wtf

--- Main.m
+++ Main.m
@@ -6,15 +6,17 @@ clear;clc;close all
 Setup() % setup plotting & paths for everything
 
 params = readParams("SizingParams.xlsx");
-
-% run all of the sizing:
+% 
+% % run all of the sizing:
 InitialVehicleSizingConstraint(params);
- 
-% TODO: Weight is both in the excel and here. UPDATE
-params.W = VehicleWeightEstimation(params);
+% 
+% % TODO: Weight is both in the excel and here. UPDATE
+% params.W = VehicleWeightEstimation(params);
+% 
+% % Drag build-up
+% params.CD_0 = DragBuildUp(params);
+% 
+% % stabilility analysis
+% A5B(params);
 
-% Drag build-up
-params.CD_0 = DragBuildUp(params);
-
-% stabilility analysis
-A5B(params);
+PropulsionSizing(params);
--- PropulsionSizing.m
+++ PropulsionSizing.m
@@ -1,28 +1,19 @@
-clear;
-clc;
-
-params = readcell("SizingParams.xlsx");
-
-N = length(params);
-
-for idx = 2:N
-    data.(params{idx,1}) = params{idx,2};
-end
+function PropulsionSizing(params)
 
 %% Thrust
-L_TO = 0.5 * data.rho * (0.7*data.V_TO)^2 * data.S_wing * data.CL_C; % Lift at takeoff [N]
-D_TO = 0.5 * data.rho * (0.7*data.V_TO)^2 * data.S_wing * (data.CD_0 + data.K_wing * data.CL_C^2); % Drag at takeoff [N]
-T_static = (data.MTOW * data.V_TO^2) / (2 * data.g * data.S_TO) + D_TO + data.mu_TO*(data.MTOW - L_TO) % Static thrust [N]
-T_C = 0.5 * data.rho * data.V_C^2 * data.S_wing * (data.CD_0 + data.K_wing * data.CL_C^2) % Cruise thrust [N]
+L_TO = 0.5 * params.env.rho * (0.7*params.performance.V_TO)^2 * params.geometry.S_wing * params.aero.CL_C; % Lift at takeoff [N]
+D_TO = 0.5 * params.env.rho * (0.7*params.performance.V_TO)^2 * params.geometry.S_wing * (params.aero.CD_0 + params.aero.K_wing * params.aero.CL_C^2); % Drag at takeoff [N]
+T_static = (params.performance.MTOW * params.performance.V_TO^2) / (2 * params.env.g * params.performance.S_TO) + D_TO + params.env.mu_TO*(params.performance.MTOW - L_TO) % Static thrust [N]
+T_C = 0.5 * params.env.rho * params.performance.V_C^2 * params.geometry.S_wing * (params.aero.CD_0 + params.aero.K_wing * params.aero.CL_C^2) % Cruise thrust [N]
 
 %% Power
 eta_motor = 0.8; % Motor efficiency
 eta_ESC = 0.95; % ESC efficiency
 
-P_shaft_total = data.MTOW / data.W_P_design; % Total shaft power [W]
+P_shaft_total = params.performance.MTOW / params.performance.W_P_design; % Total shaft power [W]
 P_shaft = P_shaft_total / 2; % Shaft power per motor [W]
-P_motor = P_shaft / eta_motor; % Motor power per motor [W]
-P_battery = P_motor / eta_ESC; % Battery power per motor [W]
+P_motor = P_shaft / eta_motor % Motor power per motor [W]
+P_battery = P_motor / eta_ESC % Battery power per motor [W]
 
 %% Analysis
 % filename = "PER3_8x6.txt";
