91bc73d2c520489f52af7784e075fbdacc361f7d
atnery <andrewtnery@gmail.com>
2026-09-18 18:17:22 -0400

    idk

--- Main.m
+++ Main.m
@@ -6,17 +6,17 @@ clear;clc;close all
 Setup() % setup plotting & paths for everything
 
 params = readParams("SizingParams.xlsx");
-% 
-% % run all of the sizing:
+
+% run all of the sizing:
 InitialVehicleSizingConstraint(params);
-% 
-% % TODO: Weight is both in the excel and here. UPDATE
-% params.W = VehicleWeightEstimation(params);
-% 
-% % Drag build-up
-% params.CD_0 = DragBuildUp(params);
-% 
-% % stabilility analysis
-% A5B(params);
+
+% TODO: Weight is both in the excel and here. UPDATE
+params.W = VehicleWeightEstimation(params);
+
+% Drag build-up
+params.CD_0 = DragBuildUp(params);
+
+% stabilility analysis
+A5B(params);
 
 PropulsionSizing(params);
