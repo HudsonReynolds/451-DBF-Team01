26471fd6cbad5a082056e90a6f84dc08a446ec84
atnery <andrewtnery@gmail.com>
2026-09-19 15:56:23 -0400

    I love Jackson Preisler

--- Main.m
+++ Main.m
@@ -7,16 +7,16 @@ Setup() % setup plotting & paths for everything
 
 params = readParams("SizingParams.xlsx");
 
-% run all of the sizing:
-InitialVehicleSizingConstraint(params);
-
-% TODO: Weight is both in the excel and here. UPDATE
-params.W = VehicleWeightEstimation(params);
-
-% Drag build-up
-params.CD_0 = DragBuildUp(params);
+% % run all of the sizing:
+% InitialVehicleSizingConstraint(params);
 % 
-% stabilility analysis
-A5B(params);
+% % TODO: Weight is both in the excel and here. UPDATE
+% params.W = VehicleWeightEstimation(params);
+% 
+% % Drag build-up
+% params.CD_0 = DragBuildUp(params);
+% % 
+% % stabilility analysis
+% A5B(params);
 
 PropulsionSizing(params);
